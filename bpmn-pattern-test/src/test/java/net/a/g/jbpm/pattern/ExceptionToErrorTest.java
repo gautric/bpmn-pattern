5f81319334578675aabd873dd0d6cebea4ff8b7b
admin <gautric@redhat.com>
2020-11-14 14:39:37 +0100

    add Process Variable

--- bpmn-pattern-test/src/test/java/net/a/g/jbpm/pattern/ExceptionToErrorTest.java
+++ bpmn-pattern-test/src/test/java/net/a/g/jbpm/pattern/ExceptionToErrorTest.java
@@ -1,19 +1,18 @@
 package net.a.g.jbpm.pattern;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import org.jbpm.test.JbpmJUnitBaseTestCase;
 import org.junit.Test;
+import org.kie.api.event.process.ProcessEventListener;
 import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.manager.RuntimeEngine;
 import org.kie.api.runtime.manager.RuntimeManager;
 import org.kie.api.runtime.process.ProcessInstance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.kie.api.event.process.ProcessCompletedEvent;
-import org.kie.api.event.process.ProcessEventListener;
-import org.kie.api.event.process.ProcessNodeLeftEvent;
-import org.kie.api.event.process.ProcessNodeTriggeredEvent;
-import org.kie.api.event.process.ProcessStartedEvent;
-import org.kie.api.event.process.ProcessVariableChangedEvent;
 
 import net.a.g.jbpm.pattern.wih.WorkItemHandlerThrowingException;
 
@@ -29,58 +28,17 @@ public class ExceptionToErrorTest extends JbpmJUnitBaseTestCase {
         final KieSession kieSession = runtimeEngine.getKieSession();
         kieSession.getWorkItemManager().registerWorkItemHandler("WorkItemHandler", new WorkItemHandlerThrowingException());
 
-        kieSession.addEventListener(new ProcessEventListener() {
-
-			private  Logger LOG = LoggerFactory.getLogger("net.a.g.jbpm.pattern.ProcessEventListener");
-
-			@Override
-			public void beforeProcessStarted(ProcessStartedEvent event) {
-				LOG.info("Start Processus : {}", event.getProcessInstance().getProcessName());
-			}
-
-			@Override
-			public void beforeProcessCompleted(ProcessCompletedEvent event) {
-			}
-
-			@Override
-			public void beforeNodeTriggered(ProcessNodeTriggeredEvent event) {
-				LOG.info("Node Called : {}", event.getNodeInstance().getNodeName());
-			}
-
-			@Override
-			public void beforeNodeLeft(ProcessNodeLeftEvent event) {
-			}
-
-			@Override
-			public void afterVariableChanged(ProcessVariableChangedEvent event) {
-			}
-
-			@Override
-			public void afterProcessStarted(ProcessStartedEvent event) {
-				LOG.info("End Processus : {}", event.getProcessInstance().getProcessName());
-
-			}
-
-			@Override
-			public void afterProcessCompleted(ProcessCompletedEvent event) {
-			}
-
-			@Override
-			public void afterNodeTriggered(ProcessNodeTriggeredEvent event) {
-			}
-
-			@Override
-			public void afterNodeLeft(ProcessNodeLeftEvent event) {
-			}
-
-			@Override
-			public void beforeVariableChanged(ProcessVariableChangedEvent event) {
-				
-			}
-		});
+        kieSession.addEventListener((ProcessEventListener)new PatternProcessListener());
 
 
-        final ProcessInstance processInstance = kieSession.startProcess("ExceptionToErrorProcess");
+		Map<String, Object> params = new HashMap<String, Object>();
+		
+		params.put("booleanIn", true);
+		params.put("stringIn", UUID.randomUUID().toString());
+		params.put("integerIn", 42);
+
+	
+		ProcessInstance processInstance = kieSession.startProcess("ExceptionToErrorProcess", params);
 
         assertProcessInstanceNotActive(processInstance.getId(), kieSession);
         assertNodeTriggered(processInstance.getId(), "ScriptTask");
