3b0644e7e9c9de5570de6d60e814c21eb61209d6
admin <gautric@redhat.com>
2020-12-06 22:40:52 +0100

    update Add process

--- bpmn-pattern-test/src/test/java/net/a/g/jbpm/pattern/AdditionTest.java
+++ bpmn-pattern-test/src/test/java/net/a/g/jbpm/pattern/AdditionTest.java
@@ -14,49 +14,42 @@ import org.kie.api.runtime.process.ProcessInstance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import net.a.g.jbpm.pattern.wih.WorkItemHandlerThrowingException;
-
 public class AdditionTest extends JbpmJUnitBaseTestCase {
-  
+
 	private static final Logger LOG = LoggerFactory.getLogger(AdditionTest.class);
 
-    
-    public AdditionTest(){
-    	super(true, true);
-    }
-    
-    @Test
-    public void testAddition() {
-    	LOG.debug("jBPM unit test sample");
-        
-        
-        Map<String, ResourceType> app = new HashMap<String, ResourceType>();
-        app.put("net/a/g/jbpm/pattern/AdditionProcess.bpmn", ResourceType.BPMN2);
-        app.put("net/a/g/jbpm/pattern/AdditionRule.dmn", ResourceType.DMN);
-        
+	public AdditionTest() {
+		super(false, false);
 
-        final RuntimeManager runtimeManager = createRuntimeManager(app);
-        
-        final RuntimeEngine runtimeEngine = getRuntimeEngine(null);
-        final KieSession kieSession = runtimeEngine.getKieSession();
+	}
 
-        kieSession.addEventListener((ProcessEventListener)new PatternProcessListener());
-        
+	@Test
+	public void testAddition() {
+		LOG.debug("jBPM unit test sample");
+
+		Map<String, ResourceType> app = new HashMap<String, ResourceType>();
+		app.put("net/a/g/jbpm/pattern/AdditionProcess.bpmn", ResourceType.BPMN2);
+		app.put("net/a/g/jbpm/pattern/AdditionRule.dmn", ResourceType.DMN);
+
+		final RuntimeManager runtimeManager = createRuntimeManager(app);
+
+		final RuntimeEngine runtimeEngine = getRuntimeEngine(null);
+		final KieSession kieSession = runtimeEngine.getKieSession();
+
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("a", 1);
 		params.put("b", 2);
 
-        final ProcessInstance processInstance = kieSession.startProcess("AdditionProcess",params);
+		final ProcessInstance processInstance = kieSession.startProcess("AdditionProcess", params);
 
-        assertProcessInstanceNotActive(processInstance.getId(), kieSession);
-        assertNodeTriggered(processInstance.getId(), "Addition DMN");
+		assertProcessInstanceNotActive(processInstance.getId(), kieSession);
+		assertNodeTriggered(processInstance.getId(), "Addition DMN");
 		assertNodeTriggered(processInstance.getId(), "Print Result");
-        assertNodeTriggered(processInstance.getId(), "Addition");
+		assertNodeTriggered(processInstance.getId(), "Addition");
 		assertNodeTriggered(processInstance.getId(), "Addition done");
 		assertProcessVarExists(processInstance, "result", "a", "b");
 
-		
-        runtimeManager.disposeRuntimeEngine(runtimeEngine);
-        runtimeManager.close();
-    }
+		runtimeManager.disposeRuntimeEngine(runtimeEngine);
+		runtimeManager.close();
+	}
 }
