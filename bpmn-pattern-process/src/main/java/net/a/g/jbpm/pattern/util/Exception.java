7114747db3a7ee14ec5461e1c90704880b48b358
admin <gautric@redhat.com>
2020-12-18 09:10:53 +0100

    fix Exception stuff

--- bpmn-pattern-process/src/main/java/net/a/g/jbpm/pattern/util/Exception.java
+++ bpmn-pattern-process/src/main/java/net/a/g/jbpm/pattern/util/Exception.java
@@ -7,8 +7,16 @@ public class Exception extends RuntimeException {
 
 	private String uuid = UUID.randomUUID().toString();
 
+	public Exception() {
+		this("Non Defined Message (please use constructor)");
+	}
+
+	public Exception(String msg) {
+		super(msg);
+	}
+
 	@Override
 	public String toString() {
-		return "Exception [uuid=" + uuid + "]";
+		return "Exception [uuid:" + uuid + ", msg:" + super.getMessage() + "]";
 	}
 }
