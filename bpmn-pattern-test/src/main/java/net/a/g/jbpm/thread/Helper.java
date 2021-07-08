5c5219668124248fb1382210e8cbe5195ef83813
admin <gautric@redhat.com>
2021-07-08 14:11:14 +0200

    Remove default

--- bpmn-pattern-test/src/main/java/net/a/g/jbpm/thread/Helper.java
+++ bpmn-pattern-test/src/main/java/net/a/g/jbpm/thread/Helper.java
@@ -9,10 +9,8 @@ public class Helper {
 	public void strackTrace() {
 
 		Logger LOG = org.slf4j.LoggerFactory.getLogger("net.a.g.jbpm.pattern.Logger");
-
-		System.setProperty("net.a.g.jbpm.filter", "");
-		
+	
 		Arrays.stream(Thread.currentThread().getStackTrace()).map(e -> "STRACKTRACE " + e.toString())
-				.filter(e -> e.contains(System.getProperty("net.a.g.jbpm.filter", "net.a"))).forEach(LOG::error);
+				.filter(e -> e.contains(System.getProperty("net.a.g.jbpm.filter", ""))).forEach(LOG::error);
 	}
 }
