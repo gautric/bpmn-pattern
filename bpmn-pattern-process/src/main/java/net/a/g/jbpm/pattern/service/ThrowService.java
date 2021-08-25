615cf61098452149d6d17958037ee4ec35f6c0ed
admin <gautric@redhat.com>
2021-08-25 11:40:13 +0200

    correct with boolean

--- bpmn-pattern-process/src/main/java/net/a/g/jbpm/pattern/service/ThrowService.java
+++ bpmn-pattern-process/src/main/java/net/a/g/jbpm/pattern/service/ThrowService.java
@@ -4,11 +4,12 @@ public class ThrowService {
 
 	public Object callMethod(Boolean throwIn) {
 
-		if (throwIn != null && throwIn) {
+		System.out.println(" Call " + this.getClass().getCanonicalName() + " " + throwIn);
+
+		if (throwIn != null && (boolean) throwIn) {
 			throw new RuntimeException("Exception inside a Service Call " + this.getClass().getCanonicalName());
 
 		}
-		System.out.println("Call " +  this.getClass().getCanonicalName());
 		return "RESULT";
 	}
 }
--- bpmn-pattern-test/src/test/java/net/a/g/jbpm/pattern/SubProcessSignalAndDrools.java
+++ bpmn-pattern-test/src/test/java/net/a/g/jbpm/pattern/SubProcessSignalAndDrools.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-import org.jbpm.process.workitem.bpmn2.ServiceTaskHandler;
+import org.jbpm.bpmn2.handler.ServiceTaskHandler;
 import org.jbpm.test.JbpmJUnitBaseTestCase;
 import org.junit.Test;
 import org.kie.api.event.process.ProcessEventListener;
@@ -95,6 +95,57 @@ public class SubProcessSignalAndDrools extends JbpmJUnitBaseTestCase {
 		}
 		
         assertProcessInstanceNotActive(processInstance.getId(), kieSession);
+		assertNodeTriggered(processInstance.getId(), "Start Processus");
+        assertNodeTriggered(processInstance.getId(), "Script");
+		assertNodeTriggered(processInstance.getId(), "Wait Timer");
+		
+		assertNodeTriggered(processInstance.getId(), "Event Switch");
+		assertNodeTriggered(processInstance.getId(), "Signal Step");
+		assertNodeTriggered(processInstance.getId(), "Delay Step");
+		assertNodeTriggered(processInstance.getId(), "Drools Step");
+		
+		assertNodeTriggered(processInstance.getId(), "Drools Path");
+		assertNodeTriggered(processInstance.getId(), "Join");
+		assertNodeTriggered(processInstance.getId(), "Process End");
+		
+        runtimeManager.disposeRuntimeEngine(runtimeEngine);
+        runtimeManager.close();
+    }
+    
+    
+    
+    @Test
+    public void testSubProcessSignalWException() {
+        SubProcessSignalAndDrools.LOG.debug("jBPM unit test sample");
+
+        final RuntimeManager runtimeManager = createRuntimeManager("net/a/g/jbpm/pattern/SubProcessSignalAndDrools.bpmn");
+        final RuntimeEngine runtimeEngine = getRuntimeEngine(null);
+        final KieSession kieSession = runtimeEngine.getKieSession();
+
+        kieSession.getWorkItemManager().registerWorkItemHandler("Service Task", new ServiceTaskHandler());
+        kieSession.addEventListener((ProcessEventListener)new PatternProcessListener());
+
+		Map<String, Object> params = new HashMap<String, Object>();
+		
+		params.put("throwIn", true);
+		params.put("stringIn", UUID.randomUUID().toString());
+		params.put("integerIn", 42);
+		params.put("timerIn", "PT2S");
+		
+		
+	
+		ProcessInstance processInstance = kieSession.startProcess("SubProcessSignalAndDrools", params);
+
+		try {
+			Thread.sleep(1000);
+			kieSession.signalEvent("SubProcessSignal", "SubProcessSignalData");
+			Thread.sleep(3000);
+
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+        assertProcessInstanceActive(processInstance.getId(), kieSession);
 		assertNodeTriggered(processInstance.getId(), "Start Processus");
         assertNodeTriggered(processInstance.getId(), "Script");
 		assertNodeTriggered(processInstance.getId(), "Wait Timer");
