e56b770bddf8e8b925441e4a374d2cae898892ef
admin <gautric@redhat.com>
2021-12-19 23:57:45 +0100

    add new Environement test

--- bpmn-pattern-test/src/test/java/net/a/g/jbpm/pattern/EnvProcessTest.java
+++ bpmn-pattern-test/src/test/java/net/a/g/jbpm/pattern/EnvProcessTest.java
@@ -7,6 +7,8 @@ import java.util.Map;
 import java.util.UUID;
 
 import org.codehaus.plexus.util.StringUtils;
+import org.jbpm.process.instance.event.DefaultSignalManagerFactory;
+import org.jbpm.process.instance.impl.DefaultProcessInstanceManagerFactory;
 import org.jbpm.ruleflow.instance.RuleFlowProcessInstance;
 import org.jbpm.test.JbpmJUnitBaseTestCase;
 import org.jbpm.workflow.instance.WorkflowProcessInstance;
@@ -14,10 +16,14 @@ import org.junit.Test;
 import org.kie.api.event.process.DefaultProcessEventListener;
 import org.kie.api.event.process.ProcessEventListener;
 import org.kie.api.event.process.ProcessStartedEvent;
+import org.kie.api.io.ResourceType;
 import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.manager.RuntimeEngine;
+import org.kie.api.runtime.manager.RuntimeEnvironment;
+import org.kie.api.runtime.manager.RuntimeEnvironmentBuilder;
 import org.kie.api.runtime.manager.RuntimeManager;
 import org.kie.api.runtime.process.ProcessInstance;
+import org.kie.internal.io.ResourceFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -64,13 +70,13 @@ public class EnvProcessTest extends JbpmJUnitBaseTestCase {
 				ret = def;
 			}
 
-			LOG.debug("name {} ret {} default {}", name, ret, def);
+			LOG.info("name : \"{}\" ret : \"{}\" default \"{}\"", name, ret, def);
 			return ret;
 		}
 	}
 
 	@Test
-	public void testWithEnv_EnvProcess() {
+	public void testWithSystemProperty_EnvProcess() {
 		EnvProcessTest.LOG.debug("jBPM unit test sample");
 
 		final RuntimeManager runtimeManager = createRuntimeManager("net/a/g/jbpm/pattern/EnvProcess.bpmn");
@@ -86,9 +92,8 @@ public class EnvProcessTest extends JbpmJUnitBaseTestCase {
 		params.put("booleanIn", true);
 		params.put("stringIn", UUID.randomUUID().toString());
 		params.put("integerIn", 42);
-		// params.put("conf", new Configuration(kieSession));
 
-		System.setProperty("net.a.g.jbpm.conf.url", "http://www.example.com");
+		System.setProperty("net.a.g.jbpm.conf.url", "http://property.example.com");
 
 		ProcessInstance processInstance = kieSession.startProcess("EnvProcess", params);
 
@@ -98,7 +103,8 @@ public class EnvProcessTest extends JbpmJUnitBaseTestCase {
 		assertNodeTriggered(processInstance.getId(), "Script Node");
 		assertNodeTriggered(processInstance.getId(), "Env Exécutée");
 
-		assertEquals(((RuleFlowProcessInstance) processInstance).getVariable("stringIn"), "http://www.example.com");
+		assertEquals(((RuleFlowProcessInstance) processInstance).getVariable("stringIn"),
+				"http://property.example.com");
 
 		runtimeManager.disposeRuntimeEngine(runtimeEngine);
 		runtimeManager.close();
@@ -131,6 +137,49 @@ public class EnvProcessTest extends JbpmJUnitBaseTestCase {
 		assertNodeTriggered(processInstance.getId(), "Env Exécutée");
 
 		assertEquals(((RuleFlowProcessInstance) processInstance).getVariable("stringIn"), "http://default.example.com");
+
+		runtimeManager.disposeRuntimeEngine(runtimeEngine);
+		runtimeManager.close();
+	}
+
+	@Test
+	public void testWithEnvironement_EnvProcess() {
+		EnvProcessTest.LOG.debug("jBPM unit test sample");
+
+		Map<String, ResourceType> resources = new HashMap<String, ResourceType>();
+
+		RuntimeEnvironment environment = RuntimeEnvironmentBuilder.Factory.get().newEmptyBuilder()
+				.addConfiguration("drools.processSignalManagerFactory", DefaultSignalManagerFactory.class.getName())
+				.addConfiguration("drools.processInstanceManagerFactory",
+						DefaultProcessInstanceManagerFactory.class.getName())
+				.addAsset(ResourceFactory.newClassPathResource("net/a/g/jbpm/pattern/EnvProcess.bpmn"),
+						ResourceType.BPMN2)
+				.addEnvironmentEntry("net.a.g.jbpm.conf.url", "http://env-entry.example.com").get();
+
+		final RuntimeManager runtimeManager = createRuntimeManager(Strategy.SINGLETON, resources, environment, "test");
+		final RuntimeEngine runtimeEngine = getRuntimeEngine(null);
+		final KieSession kieSession = runtimeEngine.getKieSession();
+
+		kieSession.getWorkItemManager().registerWorkItemHandler("Human Task", new CopyInToOutWorkItemHandler());
+		kieSession.addEventListener((ProcessEventListener) new PatternProcessListener());
+		kieSession.addEventListener((ProcessEventListener) new ConfigurationInjectorListener());
+
+		Map<String, Object> params = new HashMap<String, Object>();
+
+		params.put("booleanIn", true);
+		params.put("stringIn", UUID.randomUUID().toString());
+		params.put("integerIn", 42);
+
+		ProcessInstance processInstance = kieSession.startProcess("EnvProcess", params);
+
+		assertProcessInstanceNotActive(processInstance.getId(), kieSession);
+		assertNodeTriggered(processInstance.getId(), "Executer Env");
+		assertNodeTriggered(processInstance.getId(), "NOP");
+		assertNodeTriggered(processInstance.getId(), "Script Node");
+		assertNodeTriggered(processInstance.getId(), "Env Exécutée");
+
+		assertEquals(((RuleFlowProcessInstance) processInstance).getVariable("stringIn"),
+				"http://env-entry.example.com");
 
 		runtimeManager.disposeRuntimeEngine(runtimeEngine);
 		runtimeManager.close();
